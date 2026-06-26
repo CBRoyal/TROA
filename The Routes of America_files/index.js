81e343bd5422c6d0d9273e70f2471ccfeb35d812
CBRoyal <balascaleb2005@gmail.com>
2026-06-26 13:32:13 -0400

    Update index.js

--- The Routes of America_files/index.js
+++ The Routes of America_files/index.js
@@ -2,7 +2,7 @@ function showAlert(alertId){
     const messageAlert= document.querySelector("#", alertId);
     const collapse= new bootstrap.Collapse(messageAlert);
     collapse.show();
-};
+}
 const modalYesButton= document.querySelector("#contact-modal .btn-primary");
 modalYesButton.addEventListener("click", function(){
     const sendButton= document.querySelector("#help-button");
